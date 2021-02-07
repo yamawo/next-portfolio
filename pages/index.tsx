5b092f4e08c0c1cc9362ccdaf69e768e6c4fc380
yamawo <y7a3m1asaki@gmail.com>
2021-02-07 18:24:15 +0900

    style: style bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import "styles/index.css";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="screen"></div>
+    </>
+  );
 };
 
 export default Home;
