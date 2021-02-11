7d034315faad33eeb5091d951f7babd3c18e620e
yamawo <y7a3m1asaki@gmail.com>
2021-02-12 00:53:59 +0900

    fix: style

--- components/domain/browser/Browser.tsx
+++ components/domain/browser/Browser.tsx
@@ -5,9 +5,7 @@ import * as React from "react";
 export const Browser: React.FC = () => {
   return (
     <div className="w-full h-full flex items-center">
-      <div
-        className={`container bg-gray-200 my-0 mx-auto ${styles.height_lap} rounded-lg shadow-lg md:${styles.height_mob} md:rounded-md`}
-      ></div>
+      <div className={`bg-gray-200 ${styles.size_lap} rounded-lg shadow-lg md: container md:${styles.size_mob}`}></div>
     </div>
   );
 };
