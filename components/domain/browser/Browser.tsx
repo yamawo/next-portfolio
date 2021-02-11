9e9ff084a059b98e13629634c5f3c8564c118929
yamawo <y7a3m1asaki@gmail.com>
2021-02-11 14:38:19 +0900

    wip

--- components/domain/browser/Browser.tsx
+++ components/domain/browser/Browser.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export const Browser: React.FC = () => {
-  return <div className="w-1/2 h-1/2 bg-gray-100 "></div>;
+  return <div className="w-1/2 h-1/2 bg-gray-100"></div>;
 };
