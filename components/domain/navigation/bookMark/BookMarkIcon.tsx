870ed93143b47c5028a91780a3b023b137d7278a
yamawo <y7a3m1asaki@gmail.com>
2021-03-12 20:31:22 +0900

    refactor: fix processing in BookMarkIcon

--- components/domain/navigation/bookMark/BookMarkIcon.tsx
+++ components/domain/navigation/bookMark/BookMarkIcon.tsx
@@ -7,22 +7,24 @@ export const BookMarkIcon: React.FC = () => {
   const IMAGE_HEIGHT = 14;
   const GITHUB_URL = "https://github.com/yamawo";
   const GMAIL_URL = "mailto:y7a3m1asaki@gmail.com";
-  const bookMarkList = ["GitHub", "Gmail"];
-  const urlList = [GITHUB_URL, GMAIL_URL];
+  const bookMarkList = [
+    { label: "GitHub", url: GITHUB_URL },
+    { label: "Gmail", url: GMAIL_URL },
+  ];
 
   return (
     <>
       {bookMarkList.map((c, index) => (
-        <a key={index} href={urlList[index]} target="_blank" rel="noopener noreferrer">
+        <a key={index} href={c.url} target="_blank" rel="noopener noreferrer">
           <button className={`${styles.book_mark_button}`}>
             <Image
               className={`${styles.book_mark_img}`}
-              src={`/assets/pngs/${c.toLowerCase()}_icon.png`}
+              src={`/assets/pngs/${c.label.toLowerCase()}_icon.png`}
               alt={`${c}`}
               width={`${IMAGE_WIDTH}`}
               height={`${IMAGE_HEIGHT}`}
             />
-            <p className={`${styles.book_mark_text}`}>{c}</p>
+            <p className={`${styles.book_mark_text}`}>{c.label}</p>
           </button>
         </a>
       ))}
