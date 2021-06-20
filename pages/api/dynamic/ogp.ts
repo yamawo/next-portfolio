3cc86adc41eea6a7dc4f4ac4353a070d0cc1b4d8
yamawo <y7a3m1asaki@gmail.com>
2021-06-21 01:49:28 +0900

    feat: add ogp end point

---
+++ pages/api/dynamic/ogp.ts
@@ -0,0 +1,24 @@
+import { createCanvas } from "canvas";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const createOgp = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const WIDTH = 1200 as const;
+  const HEIGHT = 630 as const;
+  const DX = 0 as const;
+  const DY = 0 as const;
+  const canvas = createCanvas(WIDTH, HEIGHT);
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#FFF";
+  ctx.fillRect(DX, DY, WIDTH, HEIGHT);
+
+  const buffer = canvas.toBuffer();
+
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": buffer.length,
+  });
+  res.end(buffer, "binary");
+};
+
+export default createOgp;
---
+++ pages/dynamic/index.tsx
@@ -0,0 +1,30 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import React from "react";
+import { useRouter } from "next/router";
+
+const DynamicPage: NextPage = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>{router.query.dynamic}</h1>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = [...Array(10)].map((_, index) => ({
+    params: {
+      dynamic: `${index}`,
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+  };
+};
+
+export default DynamicPage;
